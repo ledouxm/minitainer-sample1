9698d5311de1c12818a98aaf1a2819dacd9df74f
ledouxm <mledoux@mledoux.fr>
2023-07-26 13:59:30 +0200

    change logs

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,11 @@ dotenv.config();
 const app = express();
 
 app.get("/", (_req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World2!");
 });
 
 const port = process.env.HTTP_PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port: ${port}`);
 });
